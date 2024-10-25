37bd5f7f59d47a54ef732ade0ef20f680e87d42f
roriwa <103991412+roriwa@users.noreply.github.com>
2024-10-25 19:52:33 +0200

    fixed size of the homepage

--- src/asciidraw/src/views/HomeView.vue
+++ src/asciidraw/src/views/HomeView.vue
@@ -5,7 +5,7 @@ import {LucideTypeOutline} from "lucide-vue-next";
 
 <template>
   <DefaultLayout>
-    <div class="min-h-screen grid place-content-center">
+    <div class="h-[calc(100vh-40px)] grid place-content-center">
       <h1 class="text-center text-4xl">AsciiDraw</h1>
       <LucideTypeOutline class="size-96" />
     </div>
