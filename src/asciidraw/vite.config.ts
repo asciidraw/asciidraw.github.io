5704ceb7ea9b535e4709ef4bc6b6315b6a291b03
roriwa <103991412+roriwa@users.noreply.github.com>
2024-10-25 18:33:49 +0200

    code cleanup

--- src/asciidraw/vite.config.ts
+++ src/asciidraw/vite.config.ts
@@ -2,9 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss"
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +14,6 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    vueJsx(),
   ],
   resolve: {
     alias: {
