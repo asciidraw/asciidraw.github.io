1b0442f70a74b66aa26cfce7f95c447cd74a752a
roriwa <103991412+roriwa@users.noreply.github.com>
2024-10-25 16:06:18 +0200

    added shadcn-vue

--- src/asciidraw/vite.config.ts
+++ src/asciidraw/vite.config.ts
@@ -3,9 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss"
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
