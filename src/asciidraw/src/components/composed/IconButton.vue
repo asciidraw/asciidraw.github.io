9c55eb0a891f2d04bf022308dc478e979ebbcc9c
roriwa <103991412+roriwa@users.noreply.github.com>
2024-10-27 09:08:03 +0100

    added new component for icon-buttons

---
+++ src/asciidraw/src/components/composed/IconButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {Button} from "@/components/ui/button";
+import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip";
+import type {TooltipContentProps} from "radix-vue";
+
+const props = defineProps<{
+  contentProps: TooltipContentProps
+}>();
+</script>
+
+<template>
+  <Button variant="ghost" size="icon" v-bind="$props">
+    <slot v-if="!$slots.tooltip" />
+    <Tooltip v-else>
+      <TooltipTrigger as-child>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent v-bind="props.contentProps">
+        <slot name="tooltip" />
+      </TooltipContent>
+    </Tooltip>
+  </Button>
+</template>
