0a93120fe1917c553eee2684fb694fde150bbb3f
roriwa <103991412+roriwa@users.noreply.github.com>
2024-10-27 13:56:09 +0100

    removed ZoomButton.vue from App.vue

--- src/asciidraw/src/app/App.vue
+++ src/asciidraw/src/app/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import AppMenu from "@/app/Menu.vue";
 import AppCanvas from "@/app/Canvas.vue";
-import AppZoomButton from "@/app/ZoomButton.vue";
 </script>
 
 <template>
   <AppMenu />
   <AppCanvas />
-  <AppZoomButton />
 </template>
