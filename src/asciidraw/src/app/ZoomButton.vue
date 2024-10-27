61e57a9356baaa6da66b8616762bba454b209015
roriwa <103991412+roriwa@users.noreply.github.com>
2024-10-27 09:08:27 +0100

    added zoom in/out button to the app (without functionality)

---
+++ src/asciidraw/src/app/ZoomButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {LucideZoomIn, LucideZoomOut} from "lucide-vue-next";
+import {Separator} from "@/components/ui/separator";
+import IconButton from "@/components/composed/IconButton.vue";
+</script>
+
+<template>
+  <div class="fixed bg-primary-foreground rounded-lg p-1 bottom-4 right-4 flex flex-col gap-y-1">
+    <IconButton :content-props="{ side: 'left' }">
+      <LucideZoomIn />
+      <template #tooltip>Zoom in</template>
+    </IconButton>
+    <Separator />
+    <IconButton :content-props="{ side: 'left' }">
+      <LucideZoomOut />
+      <template #tooltip>Zoom out</template>
+    </IconButton>
+  </div>
+</template>
