3df43a9056ed943d8d9577762430a5e1fe0edc58
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-04 22:34:04 +0100

    updated canvas to draw grid properly with zoom

--- src/asciidraw/src/constants.ts
+++ src/asciidraw/src/constants.ts
@@ -1,3 +1,12 @@
+/** width of a character in pixels */
 export const CHARACTER_PIXEL_WIDTH = 9;
+/** height of a character in pixels */
 export const CHARACTER_PIXEL_HEIGHT = 16;
+/** font used on the canvas */
 export const FONT = `${CHARACTER_PIXEL_HEIGHT-2}px Courier New`;
+/** maximum number of cells (x axis) */
+export const MAX_GRID_WIDTH = 2000;
+/** maximum number of cells (y axis) */
+export const MAX_GRID_HEIGHT = 1125;
+/** number of cells around the screen to ensure proper rendering */
+export const RENDER_PADDING_CELLS = 3;
