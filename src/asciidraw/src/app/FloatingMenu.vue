7d9f3177c81462c442e407f3b9f8bfaca2dec516
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-05 13:11:55 +0100

    added dropzone element to load a project

--- src/asciidraw/src/app/FloatingMenu.vue
+++ src/asciidraw/src/app/FloatingMenu.vue
@@ -16,8 +16,11 @@ import IconButton from "@/components/composed/IconButton.vue";
 import LocaleToggle from "@/components/LocaleToggle.vue";
 import {useEventBus} from "@vueuse/core";
 import {EVENT_DOWNLOAD_PROJECT} from "@/symbols.ts";
+import {useIsDropAvailable} from "@/composables/useIsDropAvailable.ts";
 
 const menuIsHidden = ref(false);
+
+const isDropAvailable = useIsDropAvailable();
 
 const downloadEvent = useEventBus(EVENT_DOWNLOAD_PROJECT);
 </script>
@@ -42,28 +45,33 @@ const downloadEvent = useEventBus(EVENT_DOWNLOAD_PROJECT);
       </IconButton>
     </div>
     <Separator :label="$t('app.menu.project.label')" />
-    <div class="flex gap-x-2">
-      <IconButton>
-        <LucideHardDriveUpload />
-        <template #tooltip>{{ $t('app.menu.project.import.project.tooltip') }}</template>
-      </IconButton>
-      <Separator orientation="vertical" class="h-6" />
-      <IconButton @click="downloadEvent.emit()">
-        <LucideHardDriveDownload />
-        <template #tooltip>{{ $t('app.menu.project.export.project.tooltip') }}</template>
-      </IconButton>
-      <IconButton>
-        <LucideShare2 />
-        <template #tooltip>{{ $t('app.menu.project.export.share.tooltip') }}</template>
-      </IconButton>
-      <IconButton>
-        <LucideClipboardCopy />
-        <template #tooltip>{{ $t('app.menu.project.export.clipboard.tooltip') }}</template>
-      </IconButton>
-      <IconButton>
-        <LucideImageDown />
-        <template #tooltip>{{ $t('app.menu.project.export.image.tooltip') }}</template>
-      </IconButton>
+    <div>
+      <div class="flex gap-x-2">
+        <IconButton>
+          <LucideHardDriveUpload />
+          <template #tooltip>{{ $t('app.menu.project.import.project.tooltip') }}</template>
+        </IconButton>
+        <Separator orientation="vertical" class="h-6" />
+        <IconButton @click="downloadEvent.emit()">
+          <LucideHardDriveDownload />
+          <template #tooltip>{{ $t('app.menu.project.export.project.tooltip') }}</template>
+        </IconButton>
+        <IconButton>
+          <LucideShare2 />
+          <template #tooltip>{{ $t('app.menu.project.export.share.tooltip') }}</template>
+        </IconButton>
+        <IconButton>
+          <LucideClipboardCopy />
+          <template #tooltip>{{ $t('app.menu.project.export.clipboard.tooltip') }}</template>
+        </IconButton>
+        <IconButton>
+          <LucideImageDown />
+          <template #tooltip>{{ $t('app.menu.project.export.image.tooltip') }}</template>
+        </IconButton>
+      </div>
+      <div v-if="isDropAvailable" class="border border-dashed h-20 grid place-content-center m-2">
+        {{ $t('app.menu.project.dropzone') }}
+      </div>
     </div>
     <Separator :label="$t('app.menu.actions.label')" />
     <Button variant="ghost" size="xs" class="gap-x-2">
---
+++ src/asciidraw/src/composables/useIsDropAvailable.ts
@@ -0,0 +1,13 @@
+import {type Ref, ref} from "vue";
+import {useEventListener} from "@vueuse/core";
+
+
+export function useIsDropAvailable(): Ref<boolean> {
+  const isAvailable = ref(false);
+
+  useEventListener('dragenter', () => isAvailable.value = true);
+  useEventListener('dragover', () => isAvailable.value = true);
+  useEventListener('dragleave', () => isAvailable.value = false);
+
+  return isAvailable;
+}
