25ce80e877d22880aae937a497f32c566d823987
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-07 13:00:22 +0100

    added layout for the /share/:data route

---
+++ src/asciidraw/src/views/ShareView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { watch } from "vue";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { LucideLoaderCircle } from "lucide-vue-next";
+
+const route = useRoute();
+
+watch(() => route.params, () => {
+  const rawData = route.params.data;
+  console.log(rawData);
+}, { immediate: true });
+</script>
+
+<template>
+  <DefaultLayout class="grid place-content-center p-5">
+    <Alert class="max-w-lg">
+      <LucideLoaderCircle class="size-4 animate-spin" />
+      <AlertTitle>
+        {{ $t('share.alert.title') }}
+      </AlertTitle>
+      <AlertDescription class="text-muted-foreground">
+        {{ $t('share.alert.description') }}
+      </AlertDescription>
+    </Alert>
+  </DefaultLayout>
+</template>
