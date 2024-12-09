94f6a5617ab7e8a4c16c40af5160c4b1b103a96e
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-09 10:13:16 +0100

    code reformat

--- src/asciidraw/src/main.ts
+++ src/asciidraw/src/main.ts
@@ -1,10 +1,10 @@
-import './assets/index.css'
+import './assets/index.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from '@/router'
+import App from './App.vue';
+import router from '@/router';
 import { createI18n } from "vue-i18n";
 import messages from "@intlify/unplugin-vue-i18n/messages";
 
@@ -29,7 +29,7 @@ const i18n = createI18n({
 });
 
 app.use(i18n);
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
