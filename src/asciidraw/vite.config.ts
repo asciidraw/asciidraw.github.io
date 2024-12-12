f76fee7004e3df8eadab1643c1cf1ac79ce2f9b9
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-12 13:52:49 +0100

    added packages to import markdown files as vue components

--- src/asciidraw/vite.config.ts
+++ src/asciidraw/vite.config.ts
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'node:path';
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
 import tailwindcss from 'tailwindcss'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import MarkdownPlugin from 'unplugin-vue-markdown/vite'
+import MarkdownItPrism from 'markdown-it-prism'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,10 +17,17 @@ export default defineConfig({
     }
   },
   plugins: [
-    vue(),
+    Vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
     VueI18nPlugin({
       include: resolve(__dirname, './src/locales/**'),
       runtimeOnly: false,
+    }),
+    MarkdownPlugin({
+      markdownItSetup(md) {
+        md.use(MarkdownItPrism);
+      }
     })
   ],
   resolve: {
