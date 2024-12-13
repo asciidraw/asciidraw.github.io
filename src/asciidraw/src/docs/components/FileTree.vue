5aa8eb366ce16bfb3b1c5bb8818e699ae4520df2
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-13 13:47:51 +0100

    updated design for the docs FileTree.vue

--- src/asciidraw/src/docs/components/FileTree.vue
+++ src/asciidraw/src/docs/components/FileTree.vue
@@ -2,7 +2,8 @@
 import type {Directory} from "@/docs/util.ts";
 import {Button} from "@/components/ui/button";
 import {useRoute} from "vue-router";
-import {computed} from "vue";
+import { computed } from "vue";
+import { compare_arrays } from "@/lib";
 
 defineProps<{
   parentPath: string[]
@@ -10,7 +11,7 @@ defineProps<{
 }>();
 
 const route = useRoute();
-const activePath = computed(() => route.params.path);
+const activePath = computed(() => route.params.path as string[] ?? []);
 
 function removeSuffix(fn: string) {
   return fn.replace(/\.\w+$/, "");
@@ -18,27 +19,21 @@ function removeSuffix(fn: string) {
 </script>
 
 <template>
-  <div class="border-l">
-    <div class="flex flex-col">
-      <template v-for="file in directory.files" :key="file">
-        <router-link v-if="file !== 'index.md'" :to="{ name: 'docs', params: { path: [...parentPath, file] } }">
-          <Button :variant="([...parentPath, file].every((element, index) => element === activePath?.[index])) ? 'secondary' : 'ghost'" class="justify-start">
-            {{ removeSuffix(file) }}
-          </Button>
-        </router-link>
-      </template>
-    </div>
+  <div class="border-l pl-2 flex flex-col gap-y-0.5">
+    <template v-for="file in directory.files" :key="file">
+      <router-link v-if="file !== 'index.md'" :to="{ name: 'docs', params: { path: [...parentPath, file] } }">
+        <Button :variant="compare_arrays([...parentPath, file], activePath) ? 'secondary' : 'ghost'" class="justify-start px-2 py-1">
+          {{ removeSuffix(file) }}
+        </Button>
+      </router-link>
+    </template>
     <template v-for="[name, directory] in Object.entries(directory.directories)" :key="directory">
-      <div>
-        <router-link :to="{ name: 'docs', params: { path: [...parentPath, name] } }">
-          <Button :variant="([...parentPath, name].every((element, index) => element === activePath?.[index])) ? 'secondary' : 'ghost'" class="w-full justify-start">
-            {{ name }}
-          </Button>
-        </router-link>
-        <div :style="{ paddingLeft: `${0.5 + (parentPath?.length ?? 0)*0.5}rem` }">
-          <FileTree :parent-path="[...parentPath, name]" :directory="directory" />
-        </div>
-      </div>
+      <router-link :to="{ name: 'docs', params: { path: [...parentPath, name] } }">
+        <Button :variant="compare_arrays([...parentPath, name], activePath) ? 'secondary' : 'ghost'" class="w-full justify-start px-2 py-1">
+          {{ name }}
+        </Button>
+      </router-link>
+      <FileTree :parent-path="[...parentPath, name]" :directory="directory" />
     </template>
   </div>
 </template>
--- src/asciidraw/src/lib/utils.ts
+++ src/asciidraw/src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function compare_arrays<T>(arr1: T[], arr2: T[]): boolean {
+  if (arr1.length !== arr2.length) return false;
+  return arr1.every((val, index) => arr2[index] === val);
+}
