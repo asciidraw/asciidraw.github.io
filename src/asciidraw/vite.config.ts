638e57d1a44ed7b7a1f412077d4508c3353279e3
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-13 14:12:54 +0100

    code reformat

--- src/asciidraw/vite.config.ts
+++ src/asciidraw/vite.config.ts
@@ -1,25 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 import { resolve } from 'node:path';
 
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import MarkdownPlugin from 'unplugin-vue-markdown/vite'
-import MarkdownItPrism from 'markdown-it-prism'
-import MarkdownItAnchor from 'markdown-it-anchor'
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+
+import MarkdownPlugin from 'unplugin-vue-markdown/vite';
+import MarkdownItPrism from 'markdown-it-prism';
+import MarkdownItAnchor from 'markdown-it-anchor';
 
 // https://vite.dev/config/
 export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
-    }
+    },
   },
   plugins: [
     Vue({
-      include: [/\.vue$/, /\.md$/]
+      include: [/\.vue$/, /\.md$/],
     }),
     VueI18nPlugin({
       include: resolve(__dirname, './src/locales/**'),
@@ -28,7 +29,10 @@ export default defineConfig({
     MarkdownPlugin({
       markdownItSetup(md) {
         md.use(MarkdownItPrism);
-        md.use(MarkdownItAnchor, { permalink: MarkdownItAnchor.permalink.linkInsideHeader() });
+        md.use(MarkdownItAnchor, {
+          permalink: MarkdownItAnchor.permalink.linkInsideHeader()
+        });
+
         md.renderer.rules.link_open = (tokens, idx) => {
           const href = tokens[idx].attrGet('href');
           return `<docs-link to="${href}">`;
@@ -40,15 +44,15 @@ export default defineConfig({
       transforms: {
         extraScripts: () => {
           return [
-            "import DocsLink from '@/docs/components/DocsLink.vue';"
+            "import DocsLink from '@/docs/components/DocsLink.vue';",
           ];
         },
       },
-    })
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
