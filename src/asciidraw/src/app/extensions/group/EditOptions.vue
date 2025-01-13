7e9e9d69cc877244d723b6ccd475060c05cff10b
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-13 16:19:26 +0100

    added group element

---
+++ src/asciidraw/src/app/extensions/group/EditOptions.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import * as styles from "./styles.ts";
+import type { GroupData } from ".";
+import {Input} from "@/components/ui/input";
+
+defineProps<{
+  data: GroupData,
+}>();
+</script>
+
+<template>
+  <div>
+    <Label>
+      Label
+    </Label>
+    <Input v-model:model-value.trim="data.label" />
+  </div>
+  <div>
+    <Label>
+      Style
+    </Label>
+    <Select v-model:model-value="data.style">
+      <SelectTrigger>
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        <template v-for="styleId in Object.keys(styles)" :key="styleId">
+          <SelectItem :value="styleId">
+            {{ styleId }}
+          </SelectItem>
+        </template>
+      </SelectContent>
+    </Select>
+  </div>
+</template>
---
+++ src/asciidraw/src/app/extensions/group/index.ts
@@ -0,0 +1,68 @@
+import { defineElementRenderer, defineExtension, doInRange, Layer } from "@/lib";
+import * as styles from "./styles.ts";
+import EditOptions from "./EditOptions.vue";
+import { v4 as uuid } from "uuid";
+
+
+export interface GroupData {
+  id: string
+  type: "group"
+  x: number
+  y: number
+  width: number
+  height: number
+  style: keyof typeof styles
+  label: string
+}
+
+
+export default defineExtension({
+  on: {
+    initProject(project) {
+      project.elements.push(<GroupData>{
+        id: uuid(),
+        type: "group",
+        x: -15,
+        y: 15,
+        width: 15,
+        height: 5,
+        style: "basic",
+        label: "Hello World",
+      });
+    },
+  },
+  renderer: {
+    group: defineElementRenderer<GroupData>({
+      EditComponent: EditOptions,
+      getBoundingBox(element){
+        return {
+          top: element.y,
+          left: element.x,
+          bottom: element.y + element.height,
+          right: element.x + element.width,
+        }
+      },
+      render(element) {
+        const style = styles[element.style];
+        const layer = new Layer();
+
+        layer.set(element.x, element.y, style.topLeft);
+        layer.set(element.x+element.width, element.y, style.topRight);
+        layer.set(element.x, element.y+element.height, style.bottomLeft);
+        layer.set(element.x+element.width, element.y+element.height, style.bottomRight);
+
+        doInRange(element.x + 1, element.x + element.width, (x, i) => {
+          layer.set(x, element.y, i < element.label.length ? element.label[i] : style.top);
+          layer.set(x, element.y+element.height, style.bottom);
+        });
+
+        doInRange(element.y + 1, element.y + element.height, y => {
+          layer.set(element.x, y, style.left);
+          layer.set(element.x+element.width, y, style.right);
+        });
+
+        return layer;
+      }
+    }),
+  },
+});
