2ba82c65ca13a0714084633a28a397d7d31adf64
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-14 09:48:38 +0100

    added onErrorCaptured

--- src/asciidraw/src/App.vue
+++ src/asciidraw/src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import {TooltipProvider} from "@/components/ui/tooltip";
+import {onErrorCaptured} from "vue";
+
+onErrorCaptured((error) => {
+  alert(`Critical Error (${error.name}: ${error.message})`);
+});
 </script>
 
 <template>
--- src/asciidraw/src/app/floating-menu/ExportClipboardDialog.vue
+++ src/asciidraw/src/app/floating-menu/ExportClipboardDialog.vue
@@ -21,8 +21,8 @@ function findMinMax(layer: Layer): [number, number, number, number] {
   let [minX, minY, maxX, maxY] = [Infinity, Infinity, -Infinity, -Infinity];
   layer.entries().forEach(([[x, y]]) => {
     if (x < minX) minX = x;
-    if (y < minY) minY = y;
     if (x > maxX) maxX = x;
+    if (y < minY) minY = y;
     if (y > maxY) maxY = y;
   });
   return [minX, minY, maxX, maxY];
