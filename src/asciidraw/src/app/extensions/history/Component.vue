f0e082870598b625c4e8c1842a1cfd9bebbe0c7a
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-17 18:06:44 +0100

    cleanup of history

--- src/asciidraw/src/app/extensions/history/Component.vue
+++ src/asciidraw/src/app/extensions/history/Component.vue
@@ -1,24 +1,15 @@
 <script setup lang="ts">
-import { useMagicKeys, useManualRefHistory, watchDebounced, whenever } from "@vueuse/core";
-import { inject, watch } from "vue";
+import { useDebouncedRefHistory, useMagicKeys, whenever } from "@vueuse/core";
+import { inject } from "vue";
 import { INJECTION_KEY_PROJECT } from "@/symbols.ts";
 
 const project = inject(INJECTION_KEY_PROJECT)!;
 
-const { history, commit, undo, redo } = useManualRefHistory(project, { clone: true, capacity: 1_000 });
+const { undo, redo } = useDebouncedRefHistory(project, { deep: true, debounce: 500, capacity: 1_000 });
 const { ctrl_z, ctrl_y } = useMagicKeys();
 
 whenever(ctrl_z, () => undo());
 whenever(ctrl_y, () => redo());
-
-watch(history, () => console.log({ history: history.value }));
-watch(project, () => console.log({ project: project.value }));
-
-// todo: sync/combine with saving in App.vue
-watchDebounced(project, () => {
-  console.log("Commit");
-  commit();
-}, { debounce: 500, maxWait: 1000, deep: true });
 </script>
 
 <template>
