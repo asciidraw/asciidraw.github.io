215e0bebc206b54a294f813dbf51633fe4d54782
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-22 21:32:35 +0100

    started to add translations to action components

--- src/asciidraw/src/app/extensions/box/EditOptions.vue
+++ src/asciidraw/src/app/extensions/box/EditOptions.vue
@@ -3,6 +3,7 @@ import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import * as styles from "./styles.ts";
 import type { BoxData } from ".";
+import { Badge } from "@/components/ui/badge";
 
 defineProps<{
   data: BoxData,
@@ -12,16 +13,19 @@ defineProps<{
 <template>
   <div>
     <Label>
-      Style
+      {{ $t('actions.box.edit.style.label') }}
     </Label>
     <Select v-model:model-value="data.style">
       <SelectTrigger>
         <SelectValue />
       </SelectTrigger>
       <SelectContent>
-        <template v-for="styleId in Object.keys(styles)" :key="styleId">
+        <template v-for="[styleId, style] in Object.entries(styles)" :key="styleId">
           <SelectItem :value="styleId">
-            {{ styleId }}
+            {{ $t(`actions.box.edit.style.style-names.${styleId}`) }}
+            <Badge variant="secondary" class="ml-1">
+              <pre>{{ style.topLeft }}{{ style.top.repeat(3) }}{{ style.topRight }}</pre>
+            </Badge>
           </SelectItem>
         </template>
       </SelectContent>
--- src/asciidraw/src/app/extensions/group/EditOptions.vue
+++ src/asciidraw/src/app/extensions/group/EditOptions.vue
@@ -4,6 +4,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import * as styles from "./styles.ts";
 import type { GroupData } from ".";
 import {Input} from "@/components/ui/input";
+import { Badge } from "@/components/ui/badge";
 
 defineProps<{
   data: GroupData,
@@ -26,9 +27,12 @@ defineProps<{
         <SelectValue />
       </SelectTrigger>
       <SelectContent>
-        <template v-for="styleId in Object.keys(styles)" :key="styleId">
+        <template v-for="[styleId, style] in Object.entries(styles)" :key="styleId">
           <SelectItem :value="styleId">
-            {{ styleId }}
+            {{ $t(`actions.group.edit.style.style-names.${styleId}`) }}
+            <Badge variant="secondary" class="ml-1">
+              <pre>{{ style.topLeft }}Group{{ style.top.repeat(3) }}{{ style.topRight }}</pre>
+            </Badge>
           </SelectItem>
         </template>
       </SelectContent>
