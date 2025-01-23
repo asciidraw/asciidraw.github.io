4df25905fb8d8666094b2d30b7c5e7dc3c020d64
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-23 13:51:47 +0100

    added max-h for images on the homepage

--- src/asciidraw/src/views/HomeView.vue
+++ src/asciidraw/src/views/HomeView.vue
@@ -20,14 +20,14 @@ import {LucideArrowRight, LucideArrowRightLeft} from "lucide-vue-next";
     </div>
     <div class="space-y-16">
       <div class="grid grid-cols-3 place-items-center gap-5 p-5">
-        <img :src="inEditorSrc" alt="In Editor" />
+        <img :src="inEditorSrc" alt="In Editor" class="max-h-96" />
         <div class="grid place-items-center">
           <LucideArrowRight class="size-10" />
           <p class="text-xl text-center">
             {{ $t('home.in-code.text') }}
           </p>
         </div>
-        <img :src="inCodeSrc" alt="In Code" />
+        <img :src="inCodeSrc" alt="In Code" class="max-h-96" />
       </div>
       <div class="grid place-items-center grid-cols-3 gap-5 p-5">
         <pre class="p-2 bg-border rounded-md leading-tight tracking-wide select-all text-xl font-mono">{{ examples.erm_dashed }}</pre>
