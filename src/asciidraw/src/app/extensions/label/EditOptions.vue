2cb6178016423b9c4b845d947709a8036621f714
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-29 21:29:12 +0100

    added fit-to-content button to the label edit component

--- src/asciidraw/src/app/extensions/label/EditOptions.vue
+++ src/asciidraw/src/app/extensions/label/EditOptions.vue
@@ -2,10 +2,17 @@
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import type { LabelData } from ".";
+import { Button } from "@/components/ui/button";
 
-defineProps<{
+const { data } = defineProps<{
   data: LabelData,
 }>();
+
+function fitToContent(): void {
+  const lines = data.text.split("\n");
+  data.width = lines.reduce((prev, curr) => Math.max(curr.length, prev), 0)-1;
+  data.height = lines.length-1;
+}
 </script>
 
 <template>
@@ -15,4 +22,7 @@ defineProps<{
     </Label>
     <Textarea v-model:model-value="data.text" class="max-h-[10rem] font-mono" />
   </div>
+  <Button variant="secondary" @click="fitToContent" class="w-full">
+    {{ $t('actions.label.edit.fit-to-content') }}
+  </Button>
 </template>
