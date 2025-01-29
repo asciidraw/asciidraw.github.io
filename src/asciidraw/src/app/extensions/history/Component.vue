b10ae85a15a6e0df262fb3474f58e94e04be70a4
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-29 12:38:58 +0100

    added ctrl+shift+z shortcut for undo

--- src/asciidraw/src/app/extensions/history/Component.vue
+++ src/asciidraw/src/app/extensions/history/Component.vue
@@ -7,9 +7,10 @@ const app = inject(INJECTION_KEY_APP)!;
 const project = inject(INJECTION_KEY_PROJECT)!;
 
 const { undo, redo } = useDebouncedRefHistory(project, { deep: true, debounce: 500, capacity: 1_000 });
-const { ctrl_z, ctrl_y } = useMagicKeys();
+const { ctrl_z, ctrl_shift_z, ctrl_y } = useMagicKeys();
 
 whenever(ctrl_z, () => undo());
+whenever(ctrl_shift_z, () => undo());
 whenever(ctrl_y, () => redo());
 
 app.value.events.on("undo", () => undo());
