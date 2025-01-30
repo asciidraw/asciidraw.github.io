c85b0f5687a64742c039c3eec757ee5404acf5eb
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-30 14:19:35 +0100

    improved DropZone

--- src/asciidraw/src/components/app/project-list/DropZone.vue
+++ src/asciidraw/src/components/app/project-list/DropZone.vue
@@ -4,17 +4,31 @@ import { templateRef, useDropZone } from "@vueuse/core";
 import { loadProjectData, setStorageSync, StorageType, storeProjectData } from "@/lib";
 import { v4 as uuid } from "uuid";
 
-
 const isDropAvailable = useIsDropAvailable();
 const uploadDropZoneRef = templateRef("upload-dropzone");
+
 useDropZone(uploadDropZoneRef, {
+  multiple: true,
+  dataTypes: (types) => types.every(
+      (type) => ["", "application/json"].some(allowedType => type === allowedType)
+  ),
   onDrop: (files) => {
-    files?.forEach(file => file.text().then((data) => {
-      const loaded = loadProjectData(StorageType.file, data);
-      if (loaded === null) throw Error("Project data is bad");
-      const newProjectId = uuid();
-      setStorageSync(`project-${newProjectId}`, storeProjectData(StorageType.storage, loaded));
-    }));
+    files?.forEach(file => {
+      file.text().then((data) => {
+        let loaded;
+        switch (file.type) {
+          case "application/json":
+            loaded = JSON.parse(data);
+            break;
+          default:
+            loaded = loadProjectData(StorageType.file, data);
+            break;
+        }
+        if (loaded === null) throw Error("Project data is bad");
+        const newProjectId = uuid();
+        setStorageSync(`project-${newProjectId}`, storeProjectData(StorageType.storage, loaded));
+      });
+    });
   },
 });
 </script>
