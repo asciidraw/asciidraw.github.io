cda960986426f450303cee7f68876614f3346fce
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-01 17:33:57 +0100

    extended env.d.ts

--- src/asciidraw/env.d.ts
+++ src/asciidraw/env.d.ts
@@ -10,6 +10,8 @@ type Pretty<T> = {
   [K in keyof T]: T[K]
 } & {}
 
+type JoinWith<T extends string, SEP extends string = ",", U extends string = T> = T extends any ? T | `${T}${Exclude<U, T> extends never ? "" : `${SEP}${JoinWith<Exclude<U, T>, SEP>}`}` : never;
+
 type CssCursor = "auto" | "default" | "none" | "context-menu" | "help" | "pointer" | "progress" | "wait" | "cell" | "crosshair" | "text" | "vertical-text" | "alias" | "copy" | "move" | "no-drop" | "not-allowed" | "grab" | "grabbing" | "e-resize" | "n-resize" | "ne-resize" | "nw-resize" | "s-resize" | "se-resize" | "sw-resize" | "w-resize" | "ew-resize" | "ns-resize" | "nesw-resize" | "nwse-resize" | "col-resize" | "row-resize" | "all-scroll" | "zoom-in" | "zoom-out";
 
 declare module '*.md' {
