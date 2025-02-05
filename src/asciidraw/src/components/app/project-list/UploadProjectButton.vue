61b264b1ada12a2f81eeb92a2c63360d2919a5e2
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-05 10:24:27 +0100

    code cleanup

--- src/asciidraw/src/components/app/project-list/UploadProjectButton.vue
+++ src/asciidraw/src/components/app/project-list/UploadProjectButton.vue
@@ -3,9 +3,6 @@ import IconButton from "@/components/composed/IconButton.vue";
 import { LucideHardDriveUpload } from "lucide-vue-next";
 import { useFileDialog } from "@vueuse/core";
 import { loadProjectsFromFiles } from "@/components/app/project-list/project-loader.ts";
-import { useRouter } from "vue-router";
-
-const router = useRouter();
 
 const fileDialog = useFileDialog({ accept: ".asciidraw.github.io,.io,application/json", multiple: false });
 
