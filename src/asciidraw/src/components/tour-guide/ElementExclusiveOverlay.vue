2e2302db3fe347e2be964aa2a82485c11b643d40
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-08 22:09:35 +0100

    removed transition

--- src/asciidraw/src/components/tour-guide/ElementExclusiveOverlay.vue
+++ src/asciidraw/src/components/tour-guide/ElementExclusiveOverlay.vue
@@ -9,8 +9,8 @@ const { top, right, bottom, left } = useElementBounding(() => element);
 </script>
 
 <template>
-  <div class="fixed z-40 bg-black/80 transition-[height]" :style="{ left: `0`, right: `0`, top: `0`, height: `${top}px` }" />
-  <div class="fixed z-40 bg-black/80 transition-[top,width,height]" :style="{ left: `0`, top: `${top}px`, height: `${bottom-top}px`, width: `${left}px` }" />
-  <div class="fixed z-40 bg-black/80 transition-[left,top,height]" :style="{ left: `${right}px`, top: `${top}px`, height: `${bottom-top}px`, right: `0` }" />
-  <div class="fixed z-40 bg-black/80 transition-[top]" :style="{ left: `0`, top: `${bottom}px`, bottom: `0`, right: `0` }" />
+  <div class="fixed z-40 bg-black/80" :style="{ left: `0`, right: `0`, top: `0`, height: `${top}px` }" />
+  <div class="fixed z-40 bg-black/80" :style="{ left: `0`, top: `${top}px`, height: `${bottom-top}px`, width: `${left}px` }" />
+  <div class="fixed z-40 bg-black/80" :style="{ left: `${right}px`, top: `${top}px`, height: `${bottom-top}px`, right: `0` }" />
+  <div class="fixed z-40 bg-black/80" :style="{ left: `0`, top: `${bottom}px`, bottom: `0`, right: `0` }" />
 </template>
