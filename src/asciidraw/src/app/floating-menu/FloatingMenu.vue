6565b8608e6202b4cf8bd347b32aa94600c6ae5d
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-10 16:32:11 +0100

    updated AppTourGuide.vue

--- src/asciidraw/src/app/floating-menu/FloatingMenu.vue
+++ src/asciidraw/src/app/floating-menu/FloatingMenu.vue
@@ -99,7 +99,7 @@ const menuIsHidden = ref(false);
     <Separator :label="$t('app.menu.actions.label')" />
     <div data-tour="tools" class="space-y-0.5">
       <template v-for="[actionId, action] in Object.entries(appContext.actions)" :key="action.id">
-        <Button :variant="actionId === appContext.activeActionId ? 'secondary' : 'ghost'" size="xs" class="gap-x-2 w-full" @click="appContext.activeActionId = actionId">
+        <Button :data-tour="`action-${actionId}`" :variant="actionId === appContext.activeActionId ? 'secondary' : 'ghost'" size="xs" class="gap-x-2 w-full" @click="appContext.activeActionId = actionId">
           <component v-if="action.icon" :is="action.icon" class="size-6" />
           <div v-else class="size-6 invisible" />
           <p class="grow text-left">{{ $t(action.displayName) }}</p>
--- src/asciidraw/src/components/guides/AppTourGuide.vue
+++ src/asciidraw/src/components/guides/AppTourGuide.vue
@@ -33,6 +33,38 @@ const steps = computed<GuideStep[]>(() => [
     side: "right",
   },
   {
+    selector: "action-box",
+    title: t('app.guide.action-box.title'),
+    description: t('app.guide.action-box.description'),
+    side: "right",
+    seeAlso: [{
+      label: t('app.guide.action-box.see-also-docs'),
+      ref: { name: 'docs', params: { path: ['elements', 'box'] } },
+    }],
+  },
+  {
+    selector: "action-label",
+    title: t('app.guide.action-label.title'),
+    description: t('app.guide.action-label.description'),
+    side: "right",
+    seeAlso: [{
+      label: t('app.guide.action-label.see-also-docs'),
+      ref: { name: 'docs', params: { path: ['elements', 'label'] } },
+    }],
+  },
+  {
+    selector: "action-move-view",
+    title: t('app.guide.action-move-view.title'),
+    description: t('app.guide.action-move-view.description'),
+    side: "right",
+  },
+  {
+    selector: "action-select+move",
+    title: t('app.guide.action-select+move.title'),
+    description: t('app.guide.action-select+move.description'),
+    side: "right",
+  },
+  {
     selector: "zoom",
     title: t('app.guide.zoom.title'),
     description: t('app.guide.zoom.description'),
