fdb0a9df4d53be08dd6257c3746e48ca68cf0805
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-10 15:11:53 +0100

    now using toast instead of window.alert to display critical errors

--- src/asciidraw/src/App.vue
+++ src/asciidraw/src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import {TooltipProvider} from "@/components/ui/tooltip";
-import {onErrorCaptured} from "vue";
-import { Toaster } from "@/components/ui/toast";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { onErrorCaptured } from "vue";
+import { Toaster, useToast } from "@/components/ui/toast";
+
+const { toast } = useToast();
 
 onErrorCaptured((error) => {
-  setTimeout(() => alert(`Critical Error (${error.name}: ${error.message})`));
+  toast({
+    title: "Critical Error",
+    description: `${error.name}: ${error.message}`,
+    variant: "destructive",
+  });
 });
 </script>
 
