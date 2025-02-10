eb036663730292175a03a10da2c4c003de9153c6
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-10 19:49:32 +0100

    know saving auxiliary lines to storage to keep consistent when reloading the page or switching the project

--- src/asciidraw/src/app/floating-menu/AuxiliaryLinesPopover.vue
+++ src/asciidraw/src/app/floating-menu/AuxiliaryLinesPopover.vue
@@ -3,12 +3,21 @@ import { LucideEqual, LucideEqualNot } from "lucide-vue-next";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import IconButton from "@/components/composed/IconButton.vue";
-import { inject } from "vue";
+import { computed, inject } from "vue";
 import { INJECTION_KEY_DRAW_CONTEXT } from "@/symbols.ts";
 import { useI18n } from "vue-i18n";
+import { StorageSerializers, syncRef, useLocalStorage } from "@vueuse/core";
 
 const drawContext = inject(INJECTION_KEY_DRAW_CONTEXT)!;
 const { t } = useI18n();
+
+const drawContextAuxiliaryLines = computed({
+  get: () => drawContext.value.auxiliaryLines,
+  set: (value) => { drawContext.value.auxiliaryLines = value },
+});
+const storageAuxiliaryLines = useLocalStorage<number | undefined>("auxiliary-lines", undefined, { serializer: StorageSerializers.number });
+// two-way synced storage and draw-context
+syncRef(storageAuxiliaryLines, drawContextAuxiliaryLines);
 
 type Entry = [() => string, undefined | number];
 
@@ -16,23 +25,22 @@ const entries: Entry[] = [
   [() => t('app.menu.project.auxiliary-lines.off'), undefined],
   [() => '80', 80],
   [() => '120', 120],
-]
-
+];
 </script>
 
 <template>
   <Popover>
     <PopoverTrigger as-child>
       <IconButton>
-        <component :is="drawContext.auxiliaryLines === undefined ? LucideEqualNot : LucideEqual" class="rotate-90" />
+        <component :is="storageAuxiliaryLines === undefined ? LucideEqualNot : LucideEqual" class="rotate-90" />
         <template #tooltip>{{ $t('app.menu.project.auxiliary-lines.tooltip') }}</template>
       </IconButton>
     </PopoverTrigger>
     <PopoverContent class="flex gap-x-2 w-fit">
       <template v-for="[label, value] in entries" :key="value">
         <Button
-          :variant="drawContext.auxiliaryLines === value ? 'secondary' : 'outline'"
-          @click="() => { drawContext.auxiliaryLines = value }"
+          :variant="storageAuxiliaryLines === value ? 'secondary' : 'outline'"
+          @click="() => { storageAuxiliaryLines = value }"
         >
           {{ label() }}
         </Button>
