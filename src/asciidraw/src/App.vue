31e8c67f5bf6aad5931de4ff460ee816a928d7b7
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-10 15:11:00 +0100

    added the <Toaster/> to App.vue

--- src/asciidraw/src/App.vue
+++ src/asciidraw/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {TooltipProvider} from "@/components/ui/tooltip";
 import {onErrorCaptured} from "vue";
+import { Toaster } from "@/components/ui/toast";
 
 onErrorCaptured((error) => {
   setTimeout(() => alert(`Critical Error (${error.name}: ${error.message})`));
@@ -11,4 +12,5 @@ onErrorCaptured((error) => {
   <TooltipProvider>
     <router-view :key="$route.fullPath" />
   </TooltipProvider>
+  <Toaster />
 </template>
