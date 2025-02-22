3f59667f0bb58467d8f73e2afca3cd9609a99367
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-22 12:58:17 +0100

    created LayerCache class

--- src/asciidraw/src/lib/index.ts
+++ src/asciidraw/src/lib/index.ts
@@ -2,6 +2,7 @@ export * from "./defines";
 export * from "./download";
 export * from "./elements";
 export * from "./layer";
+export * from "./layerCache.ts";
 export * from "./storage";
 export * from "./utils";
 export * from "./vector";
