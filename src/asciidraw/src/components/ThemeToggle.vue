f30b7dc97e5b4bfe36305682d2c6a2e454e02051
roriwa <103991412+roriwa@users.noreply.github.com>
2025-02-24 20:47:00 +0100

    ThemeToggle.vue icon is now the same as the theme-icon

--- src/asciidraw/src/components/ThemeToggle.vue
+++ src/asciidraw/src/components/ThemeToggle.vue
@@ -24,11 +24,7 @@ const { store: colorMode } = useConfiguredColorMode();
   <DropdownMenu>
     <DropdownMenuTrigger as-child>
       <IconButton>
-        <div class="grid place-content-center">
-          <LucideSun class="col-[1/1] row-[1/1] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <LucideMoon class="col-[1/1] row-[1/1] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span class="sr-only">Toggle theme</span>
-        </div>
+        <component :is="colorModes.find(c => c.id === colorMode)?.icon ?? LucideSunMoon" />
         <template #tooltip>{{ $t('components.theme-toggle.tooltip') }}</template>
       </IconButton>
     </DropdownMenuTrigger>
